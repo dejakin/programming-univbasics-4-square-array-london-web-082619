f6adf9fbe74a9b8ec586c145dbf52435bfca0007
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-08-17 19:37:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   new_numbers = []
   counter = 0
   while counter < array.length do
-  puts array[counter]**2
+  new_numbers.push(array[counter]**2)
   counter += 1
   end
 end
