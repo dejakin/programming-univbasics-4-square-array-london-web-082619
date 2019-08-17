b826a888b987e102f07c6a4c2ee46fc2d9e2a868
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-08-17 19:26:48 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,9 +1,10 @@
 numbers = [1, 2, 3, 4, 5]
 sqnum = []
+
 def square_array(array)
   counter = 0
   while counter < array.length do
-  puts array[counter]**2
+  sqnum.push(array[counter]**2)
   counter += 1
   end
 end
