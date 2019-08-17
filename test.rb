bbf2b320869bd87ff3ac716f4df22c6263232237
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-08-17 19:23:41 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -3,8 +3,9 @@ numbers = [1, 2, 3, 4, 5]
 def square_array(array)
   counter = 0
   while counter < array.length do
-  puts array[counter]
+  puts array[counter]**2
   counter += 1
+  end
 end
 
 square_array(numbers)
